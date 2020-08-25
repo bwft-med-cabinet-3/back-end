dc87fcf5db13f01a9c79e2a3950cc0b901933960
oscar-lopez-projects <d1lopeztv@gmail.com>
2020-08-24 21:09:58 -0700

    now working on config and controllers

--- src/main/java/com/lambdaschool/backend/exceptions/ResourceFoundException.java
+++ src/main/java/com/lambdaschool/backend/exceptions/ResourceFoundException.java
@@ -1,4 +1,13 @@
 package com.lambdaschool.backend.exceptions;
 
-public class ResourceFoundException {
+/**
+ * A custom exception to be used when a resource is found but is not suppose to be
+ */
+public class ResourceFoundException
+        extends RuntimeException
+{
+    public ResourceFoundException(String message)
+    {
+        super("Error from a Lambda School Application " + message);
+    }
 }
